aa6f3f76e284709960ac70a913987936b9e1fae3
jbreaker1 <jpatel8@lion.lmu.edu>
2017-02-08 23:40:11 -0800

    added some more tests

--- test/whiteboard-test.js
+++ test/whiteboard-test.js
@@ -11,14 +11,19 @@ let test = (f) => {
     process.exitCode = 1;
   }
 }
+var ifStatement = "if (1 == 1):    return true."
+var ifStatementFail = "if (1 == 1):    return true"
 var string2 = "x[]"
 var strings = ['1','2'];
 var stringx = 'Hello World';
 var testArray = [1,2,3];
 var testArray2 = [1,'b','a'];
-test(grammar.match(strings));
-test(grammar.match("5"));
-test(grammar.match("x["));
-test(grammar.match(stringx));
-test(grammar.match(testArray2));
-test(grammar.match(string2));
+
+test(grammar.match(strings)); // (success) can have a list of numbers
+test(grammar.match("5")); // (success) takes in the number 5
+test(grammar.match("x[")); // (error) can't do x[
+test(grammar.match(stringx)); // (success) strings with a space
+test(grammar.match(testArray2)); // (success) Array that takes in multiple values 
+test(grammar.match(string2)); // (error) cant do x[]
+test(grammar.match(ifStatement)); // (success) if statement 
+test(grammar.match(ifStatementFail)); // (error) missing a period
