0a048e2090fc0908130b5f9841e5257307b0b4a8
kmetelus <kmetelus@lion.lmu.edu>
2017-02-09 12:15:59 -0800

    npm test runs tests on expressions

--- test/whiteboard-test.js
+++ test/whiteboard-test.js
@@ -1,36 +1,36 @@
+const assert = require('assert');
 const fs = require('fs');
 const ohm = require('ohm-js');
-const language = fs.readFileSync('../whiteboard.ohm');
+const language = fs.readFileSync('whiteboard.ohm');
 const grammar = ohm.grammar(language);
 
-let test = (f) => {
-  if (f.succeeded()) {
-    console.log("success");
-  } else {
-    console.error("error");
-    process.exitCode = 1;
-  }
+let test = (f, e) => {
+  let x = f.succeeded() ? true : !e;
+  return x;
 }
-var ifStatement = "if (1 == 1):    return true."
-var ifStatementFail = "if (1 == 1):    return true"
+//
+var ifstmt = "if (1 == 1): .";
 var string2 = "x[]"
 var strings = ['1','2'];
 var stringx = 'Hello World';
 var testArray = [1,2,3];
 var testArray2 = [1,'b','a'];
-var funct = "fun helloworld = ():    for i in range(5):    x . ."
-var funct2 = "fun helloworld = ()    for i in range(5):    x . ."
-var adder = "1 + 2";
-var subtract = "2-1";
-test(grammar.match(strings)); // (success) can have a list of numbers
-test(grammar.match("5")); // (success) takes in the number 5
-test(grammar.match("x[")); // (error) can't do x[
-test(grammar.match(stringx)); // (success) strings with a space
-test(grammar.match(testArray2)); // (success) Array that takes in multiple values 
-test(grammar.match(string2)); // (error) cant do x[]
-test(grammar.match(ifStatement)); // (success) if statement 
-test(grammar.match(ifStatementFail)); // (error) missing a period
-test(grammar.match(funct)); // (success) function works
-test(grammar.match(funct2)); // (error) missing : after ()
-test(grammar.match(adder)); // (success) adding 1 + 2
-test(grammar.match(subtract)); // (success) subtract 2-1
+var aTrueTest = "if (num < 2 or num > LARGEST): \
+        x = 4 \
+    ."
+
+
+describe('Grammar', () => {
+  it('Tests to see if certain expressions match to syntax tree', done => {
+    assert.equal(true, true);
+    assert.equal(test(grammar.match(strings),true), true);
+    assert.equal(test(grammar.match("5"), true), true);
+    assert.equal(test(grammar.match("x]"), false), true);
+    assert.equal(test(grammar.match(stringx), true), true);
+    assert.equal(test(grammar.match(testArray2), false), true);
+    assert.equal(test(grammar.match(string2), false), true);
+    assert.equal(test(grammar.match(ifstmt), true), true);
+    assert.equal(test(grammar.match(aTrueTest), true), true);
+    done();
+  });
+});
