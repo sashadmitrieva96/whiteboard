5d62eac2ca7ae5323d7020eb54303d22259da8f2
kmetelus <kmetelus@lion.lmu.edu>
2017-02-08 23:01:49 -0800

    added some preliminary tests

--- test/whiteboard-test.js
+++ test/whiteboard-test.js
@@ -1,1 +1,19 @@
-const assert = require('assert');
+const fs = require('fs');
+const ohm = require('ohm-js');
+const language = fs.readFileSync('../whiteboard.ohm');
+const grammar = ohm.grammar(language);
+
+let test = (f) => {
+  if (f.succeeded()) {
+    console.log("success");
+  } else {
+    console.error("error");
+    process.exitCode = 1;
+  }
+}
+
+var strings = ['1','2'];
+
+test(grammar.match(strings));
+test(grammar.match("5"));
+test(grammar.match("x["));
