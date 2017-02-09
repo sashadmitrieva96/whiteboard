d5a101ca39125a61855d0400cf9c5a99f86b1cc6
kmetelus <kmetelus@lion.lmu.edu>
2017-02-08 22:32:25 -0800

    added test file

--- whiteboard.js
+++ whiteboard.js
@@ -2,3 +2,11 @@ const fs = require('fs');
 const ohm = require('ohm-js');
 const language = fs.readFileSync('whiteboard.ohm');
 const grammar = ohm.grammar(language);
+
+let match = grammar.match(process.argv[2]);
+if (match.succeeded()) {
+  console.log("success");
+} else {
+  console.error("error");
+  process.exitCode = 1;
+}
