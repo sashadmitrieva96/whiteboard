3dfca1b6ba5e04ec09312acf1f7d9f9fe33ea2cf
RyTaus <rtaus1@lion.lmu.edu>
2017-03-01 20:32:21 -0800

    newest test cases

--- test/whiteboard-test.js
+++ test/whiteboard-test.js
@@ -4,21 +4,6 @@ const ohm = require('ohm-js');
 const language = fs.readFileSync('whiteboard.ohm');
 const grammar = ohm.grammar(language);
 
-let test = (f, e) => {
-  let x = f.succeeded() ? true : !e;
-  return x;
-}
-//
-var ifstmt = "if (1 == 1): .";
-var string2 = "x[]"
-var strings = ['1','2'];
-var stringx = 'Hello World';
-var testArray = [1,2,3];
-var testArray2 = [1,'b','a'];
-var aTrueTest = "if (num < 2 or num > LARGEST): \
-        x = 4 \
-    ."
-
 let positiveTests = [
     `if true == ryan: .`,
     `array[3].funcall(p1, p2)`,
@@ -31,6 +16,7 @@ let positiveTests = [
     .`,
     `a[0] or true and ('baller' >= area[2](a, b, c))`,
     `2534.7654`,
+    `Num x = 00.7654`,
     ` `
 
 
@@ -48,14 +34,11 @@ let negativeTests = [
         width * height
         .
     .`,
-    `true == (0are)`
+    `true == (0are)`,
+    `(num x = 00.7654)`
 
 
 ]
-
-
-
-
 
 describe('Grammar', () => {
   it('matches with programs it should', () => {
@@ -70,18 +53,3 @@ describe('Grammar', () => {
     }
   });
 });
-
-// describe('Grammar', () => {
-//   it('Tests to see if certain expressions match to syntax tree', done => {
-//     assert.equal(true, true);
-//     assert.equal(test(grammar.match(strings),true), true);
-//     assert.equal(test(grammar.match("5"), true), true);
-//     assert.equal(test(grammar.match("x]"), false), true);
-//     assert.equal(test(grammar.match(stringx), true), true);
-//     assert.equal(test(grammar.match(testArray2), false), true);
-//     assert.equal(test(grammar.match(string2), false), true);
-//     assert.equal(test(grammar.match(ifstmt), true), true);
-//     assert.equal(test(grammar.match(aTrueTest), true), true);
-//     done();
-//   });
-// });
