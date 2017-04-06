76c18859a10cca58c2e1c2fd4018daff2119a02b
pokeadotproductions <zfitzpat@lion.lmu.edu>
2017-04-06 15:52:21 -0700

    Changes made

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ $.get('whiteboard.ohm', function(data) {
     $('#grammar-row').append('<pre>' + data + '</pre>');
 });
 
+$.get('all_sample_code.md', function(data) {
+    console.log("here");
+    $('#sample-code-row').append('<pre>' + data + '</pre>');
+});
+
 $('.tab').click(function(tab) {
     let tabClasses = tab.currentTarget.className;
     let tabClass = (tabClasses).substr('0', tabClasses.indexOf(' '));
