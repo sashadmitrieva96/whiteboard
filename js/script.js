0d5be4a6a56a38207ade41003adf3c95663fb3d1
aherco <aherco@lion.lmu.edu>
2017-04-06 21:38:59 -0700

    CSS hacks

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-$.get('whiteboard.ohm', function(data) {
-    $('#grammar-row').append('<pre>' + data + '</pre>');
+$.get('whiteboard.ohm', (data) => {
+  $('#grammar-row').append('<pre>' + data + '</pre>');
 });
 
-$.get('all_sample_code.md', function(data) {
-    console.log("here");
-    $('#sample-code-row').append('<pre>' + data + '</pre>');
+$.get('all_sample_code.md', (data) => {
+  $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
-$('.tab').click(function(tab) {
-    let tabClasses = tab.currentTarget.className;
-    let tabClass = (tabClasses).substr('0', tabClasses.indexOf(' '));
-    $('.row').removeClass('active').addClass('inactive');
-    $('#' + tabClass + '-row').removeClass('inactive').addClass('active');
+$('.tab').click((tab) => {
+  const tabClasses = tab.currentTarget.className;
+  const tabClass = (tabClasses).substr('0', tabClasses.indexOf(' '));
+  $('.row').removeClass('active').addClass('inactive');
+  $('#' + tabClass + '-row').removeClass('inactive').addClass('active');
 });
