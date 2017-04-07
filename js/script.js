b6b3d580546823de3aad9e4d3fb575e69a48a54f
pokeadotproductions <zfitzpat@lion.lmu.edu>
2017-04-06 23:33:30 -0700

    Added descriptions to sample code

--- js/script.js
+++ js/script.js
@@ -9,18 +9,14 @@ $.get('./Sample Code/hello_world_sample.md', (data) => {
   $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
-$.get('./Sample Code/cat_sample.md', (data) => {
-  $('#sample-code-row').append('<div id="description"><h4><br></h4></div>');
-  $('#sample-code-row').append('<pre>' + data + '</pre>');
-});
-
 $.get('./Sample Code/if_sample.md', (data) => {
   $('#sample-code-row').append('<div id="description"><h4>If Statements<br></h4></div>');
   $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
 $.get('./Sample Code/loop_sample.md', (data) => {
-  $('#sample-code-row').append('<div id="description"><h4>Loops<br></h4></div>');
+  $('#sample-code-row').append(`<div id="description"><h4>Loops<br></h4>In whiteboard, you are able to use for and foreach loops to iterate over 
+    any iterable object. This includes the ordered types lists, dictionaries, sets, stacks, and any UserType that is iterable and ordered. </div>`);
   $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
@@ -30,17 +26,18 @@ $.get('./Sample Code/function_sample.md', (data) => {
 });
 
 $.get('./Sample Code/exception_sample.md', (data) => {
-  $('#sample-code-row').append('<div id="description"><h4>Exceptions<br></h4></div>');
+  $('#sample-code-row').append('<div id="description"><h4>Exceptions<br></h4>Exceptions are thrown with a `Error` message, shown below:</div>');
   $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
 $.get('./Sample Code/comment_sample.md', (data) => {
-  $('#sample-code-row').append('<div id="description"><h4>Comments<br></h4></div>');
+  $('#sample-code-row').append(`<div id="description"><h4>Comments<br></h4>Single line comments in whiteboard begin with a single hash \`#\`...<br>
+    ...while multi-line comments begin and end with a double hash \`##\`.</div>`);
   $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
 $.get('./Sample Code/whiteboard_sample.md', (data) => {
-  $('#sample-code-row').append('<div id="description"><h4>Example Programs<br></h4></div>');
+  $('#sample-code-row').append('<div id="description"><h4>Example Programs<br></h4>A series of programs in whiteboard with the equivalent Javascript underneath for comparison.</div>');
   $('#sample-code-row').append('<pre>' + data + '</pre>');
 });
 
